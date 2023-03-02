9d3fa19c5053b90d76dd0ef98a01b2a0180b6ef1
GabsCastroN <gabrielcastro@colegioguanenta.edu.co>
2023-03-02 18:13:11 -0500

    se agrega el segundo punto del talle con py y diagrama

---
+++ Prestamo_Bancario/Prestamo.py
@@ -0,0 +1,14 @@
+#input
+X = int(input("Cantidad de dinero que ganas: "))
+
+if X > 945201:
+    Z = int(input("Cuantas deudas tienes? "))
+    if Z == 0:
+        print ("prestamo aprobado")
+    else:
+        print ("prestamo no aprobado")
+
+
+
+
+
--- Prestamo_Bancario/README.md
+++ Prestamo_Bancario/README.md
@@ -1,5 +1,3 @@
-Programa para que lea las coordenadas cartesianas
-
 # Diagrama de flujo
 
 ![Diagrama de flujo](diagrama.png "diagrama de flujo")
